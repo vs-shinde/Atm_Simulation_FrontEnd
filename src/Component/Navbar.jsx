10446868c93ef666bff19f78fa863dba50d984e0
anjali-agambare <Anjali.10825422@LTIMindtree.com>
2024-11-28 12:37:47 +0530

    add hsbc logo on  navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import hsbscLogo from "../Assets/hsbcLogo.png";
 
 const NavbarComponent = ({ username }) => {
   return (
     <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#db0011' }}>
       <div className="container-fluid">
         <a className="navbar-brand" href="/" style={{ color: '#ffffff', fontWeight: 'bold' }}>HSBC ATM</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <div style={{marginRight:"1rem"}}>
+        <img
+          src={hsbscLogo}
+          style={{
+            height: "28px",
+            width: "38px",
+          }}
+        />
+      </div>
+        {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           
-        </div>
+        </div> */}
       </div>
     </nav>
   );
